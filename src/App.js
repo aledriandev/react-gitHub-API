528d29246b7631f51ef68cb57f2d399960bb32ce
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-12 13:49:43 -0500

    Instalando depenencias

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactBootstrap from 'react-bootstrap';
+import {
+	BrowserRouter,
+	Route,
+	Switch,
+	NavLink,
+	Redirect
+} from 'react-router-dom'
+import { Grid, Col, Row, InputGroup, FormControl ,Dropdown, MenuItem , Tabs , Tab} from 'react-bootstrap';
+
+class Home extends Component {
+  render() {
+    return (
+      <h2>HOME</h2>
+    );
+  }
+}
+
+class Battle extends Component {
+  render() {
+    return (
+      <h2>BATTLE</h2>
+    );
+  }
+}
+
+class Popular extends Component {
+  render() {
+    return (
+      <h2>POPULAR</h2>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+      <div>
+        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+          <Tab eventKey={1} title="Home">
+            {/* <NavLink to="/home"></NavLink> */}
+          </Tab>
+          <Tab eventKey={2} title="Battle">
+
+          {/* <NavLink to="/battle"></NavLink> */}
+          </Tab>
+          <Tab eventKey={3} title="Popular">
+
+          {/* <NavLink to="/popular"></NavLink> */}
+          </Tab>
+        </Tabs>
       </div>
+          <div>
+            <Switch>
+              <Route exact path="/"
+                  render={() => <Redirect to= {'/home'}/>}/>
+              <Route path="/Proyecto-Lyft"
+                render={() => <Redirect to= {'/home'}/>}/>
+              <Route  path="/home" render={() => <Home />}/>
+              <Route  path="/battle" render={() => <Battle />}/>
+              <Route  path="/popular" render={() => <Popular />}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
     );
   }
 }
